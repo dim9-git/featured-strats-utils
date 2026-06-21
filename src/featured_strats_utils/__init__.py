5b6b46c6c152959a2f0fa90ea6761df045637d5d
Dimash <dimash@Dimashs-MacBook-Air.local>
2026-06-21 13:37:23 +0500

    fix: futures_ccxt file renamed

--- src/featured_strats_utils/__init__.py
+++ src/featured_strats_utils/__init__.py
@@ -1,5 +1,5 @@
 from .binance_metrics import BinanceMetricsParams, fetch_binance_metrics_df
-from .features_ccxt import CcxtOiParams, fetch_open_interest_df
+from .futures_ccxt import CcxtOiParams, fetch_open_interest_df
 from .spot_ccxt import CcxtParams, fetch_ccxt_df
 from .dataframe import sparse_cooldown, rolling_zscore, load_daily_json_data
 
