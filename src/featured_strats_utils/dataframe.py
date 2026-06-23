93883267af6d29bdac366a5d72047000eacd171d
Dimash <dimash@Dimashs-MacBook-Air.local>
2026-06-23 10:48:03 +0500

    fix: import in blockchain

--- src/featured_strats_utils/dataframe.py
+++ src/featured_strats_utils/dataframe.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import requests
-from pathlib import Path
 
 def ensure_datetime_index(df: pd.DataFrame) -> pd.DataFrame:
     if not isinstance(df.index, pd.DatetimeIndex):
